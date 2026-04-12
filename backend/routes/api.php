f129e69090b33b03308ec613f60a25940d8174d8
Adam Amttout <adamamttout@gmail.com>
2026-04-12 17:53:26 +0100

    modifier la base de donnees

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,18 +11,15 @@ Route::get('/test', fn() => ['message' => 'API OK']);
 // AUTH
 Route::post('/register-user', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-
 // CLIENTS
 Route::get('/clients', [ClientController::class, 'index']);
 Route::get('/clients/{id}', [ClientController::class, 'show']);
 Route::delete('/clients/{id}', [ClientController::class, 'destroy']);
-
+Route::post('/clients', [ClientController::class, 'store']);
+Route::put('/clients/{id}', [ClientController::class, 'update']);
 // INSCRIPTIONS
 Route::get('/inscriptions', [InscriptionController::class, 'index']); 
 Route::post('/inscription', [InscriptionController::class, 'store']);
-
-Route::put('/clients/{id}', [ClientController::class, 'update']);
 Route::delete('/inscriptions/{id}', [InscriptionController::class, 'destroy']);
 Route::put('/inscriptions/{id}', [InscriptionController::class, 'update']);
 
-Route::post('/clients', [ClientController::class, 'store']);
